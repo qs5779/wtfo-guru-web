def7fa8cf40484fd2d8f967185225f4d785f63bb
Quien Sabe <qs5579@mail.com>
2023-09-25 14:55:41 -0400

    feat: More improvements

--- app/library/helpers.py
+++ app/library/helpers.py
@@ -1,13 +1,14 @@
-import os
+from pathlib import Path
 
 import markdown
 
 
 def openfile(filename: str) -> dict[str, str]:
     """Return dictionary with key text equal to html for page."""
-    filepath = os.path.join("app/pages/", filename)
+    filepath = Path("app/pages") / filename
     with open(filepath, "r", encoding="utf-8") as input_file:
         text = input_file.read()
 
+    title = filepath.name.replace(".md", "").capitalize()
     html = markdown.markdown(text, extensions=["fenced_code"])
-    return {"text": html}
+    return {"text": html, "title": title}
