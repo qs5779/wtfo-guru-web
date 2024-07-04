cf1ea6230de00df4c679bf04aa6a42d42b441c68
Quien Sabe <qs5779@mail.com>
2024-07-04 15:03:48 -0400

    1.0.3 dev1 (#75)
    * bump: 1.0.3-dev1
    
    * docs: New style changelog
    
    * fix: 1.0.3-dev1 see CHANGELOG.md
    
    ---------
    
    Co-authored-by: Quien Sabe <qs5579@mail.com>

--- main.py
+++ main.py
@@ -17,17 +17,11 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 async def home(request: Request):
     """Return html for home page."""
     file_data = openfile("home.md")
-    return templates.TemplateResponse(
-        "page.html",
-        {"request": request, "data": file_data},
-    )
+    return templates.TemplateResponse(request, "page.html", {"data": file_data})
 
 
 @app.get("/page/{page_name}", response_class=HTMLResponse)
 async def show_page(request: Request, page_name: str):
     """Return html for page identified by  page_name."""
     file_data = openfile("{0}.md".format(page_name))
-    return templates.TemplateResponse(
-        "page.html",
-        {"request": request, "data": file_data},
-    )
+    return templates.TemplateResponse(request, "page.html", {"data": file_data})
